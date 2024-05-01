6ed65f9a83543e9a15cb7fed25d656aa7df0bde2
LeoStrange <50527902+LeoStrange@users.noreply.github.com>
2024-05-01 20:40:22 -0300

    a

--- SGE/SGE.Aplicacion/Validadores/ExpedienteValidador.cs
+++ SGE/SGE.Aplicacion/Validadores/ExpedienteValidador.cs
@@ -1,2 +1,20 @@
 namespace SGE.Aplicacion;
 
+class ExpedienteValidador
+{
+
+    public static bool CaratulaValidador(string caratula)
+    {
+
+        if(caratula == "")
+        {
+            return true;
+        }
+        else
+        {
+            throw new ValidacionException("La carátula no puede estar vacía.");
+        }
+
+    }
+
+}
