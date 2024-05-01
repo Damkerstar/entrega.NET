311f0a06bff5bc2db5e0a8f112bb144c18c8762c
Damkerstar <damyoutu@gmail.com>
2024-05-01 20:16:26 -0300

    add

--- SGE/SGE.Aplicacion/Entidades/Expediente.cs
+++ SGE/SGE.Aplicacion/Entidades/Expediente.cs
@@ -1,15 +1,14 @@
 namespace SGE.Aplicacion;
 
-class Expediente
-{
+class Expediente{
 
     private static int s_id { get; set; } = 0;
     public int  _id {get;}
     private string _caratula {get;}
-    private DateTime _fechaYHoraCreacion {get;}
+    private DateTime _fechaYHoraCreacion {get;} = DateTime.Now;
     private DateTime _fechaYHoraActualizacion {get;}
     private int _usuarioID {get;}
-    private EstadoExpediente _estado {get;}
+    private EstadoExpediente _estado {get;} = EstadoExpediente.Recien_Iniciado;
 
     public Expediente(string caratula, int usuarioID) 
     {
@@ -17,10 +16,8 @@ class Expediente
         s_id++;
         _id = s_id;
         this._caratula = caratula;
-        this._fechaYHoraCreacion = DateTime.Now;
         this._fechaYHoraActualizacion = this._fechaYHoraCreacion;
         this._usuarioID = usuarioID;
-        this._estado = EstadoExpediente.Recien_Iniciado;
 
     }
 
--- SGE/SGE.Aplicacion/Entidades/Tramite.cs
+++ SGE/SGE.Aplicacion/Entidades/Tramite.cs
@@ -4,14 +4,25 @@ public class Tramite
 {
 
     //Hacer propiedad ExpedienteId
+    /*
+    public int ExpedienteId {get; set;}
+    */
     private static int s_id {get; set;} = 0;
     private int _id {get;}
+    private Etiqueta_Tramite _etiqueta {get;} = Etiqueta_Tramite.Recien_Iniciado;
+    private string _descripcion {get;};
+    private DateTime _fechaYhoraCreacion{get;} = DateTime.Now;
+    private DateTime _fechaYhoraModificacion {get;} = _fechaYhoraCreacion;
+    private int _idUsuario;
 
-    public Tramite() 
+
+    public Tramite(string descripcion, int idUsuario) 
     {
 
-        s_id++;
-        _id = s_id;
+        this.s_id++;
+        this._id = s_id;
+        this._descripcion = descripcion;
+        this._idUsuario = idUsuario;
 
     }
 
