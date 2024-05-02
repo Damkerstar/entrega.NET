79426ea749b8424a67683c070a6062928f47e62e
Damkerstar <damyoutu@gmail.com>
2024-05-01 22:23:31 -0300

    d

--- SGE/SGE.Aplicacion/Interfaces/ITramiteRepositorio.cs
+++ SGE/SGE.Aplicacion/Interfaces/ITramiteRepositorio.cs
@@ -2,10 +2,8 @@ namespace SGE.Aplicacion;
 
 public interface ITramiteRepositorio{
     public void agregarTramite(Tramite tramite);
-<<<<<<< HEAD
     public void eliminarTramite(Tramite tramite);
     public void eliminarCompleto(int idE);
-=======
 
->>>>>>> 647f4061424ed0dcde365c07158fb9e68ed4c8f5
+
 }
