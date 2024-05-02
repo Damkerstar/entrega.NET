b2e687b97aefb10b5344a27bb0e9c8a9f4299573
Damkerstar <damyoutu@gmail.com>
2024-05-01 22:29:21 -0300

    d

--- SGE/SGE.Aplicacion/CasosDeUso/CasoDeUsoExpedienteBaja.cs
+++ SGE/SGE.Aplicacion/CasosDeUso/CasoDeUsoExpedienteBaja.cs
@@ -5,7 +5,7 @@ public class CasoDeUsoExpedienteBaja(IExpedienteRepositorio repo)
 
     public void Ejecutar(Expediente e)
     {
-
+        // PREGUNTAR?
         CasoDeUsoTramiteBaja.Ejecutar(e.ID);
 
         repo.EliminarExpediente(e);
--- SGE/SGE.Aplicacion/CasosDeUso/CasoDeUsoTramiteBaja.cs
+++ SGE/SGE.Aplicacion/CasosDeUso/CasoDeUsoTramiteBaja.cs
@@ -1,14 +1,15 @@
 namespace SGE.Aplicacion;
-public class CasoDeUsoTramiteBaja(ITramiteRepositorio repoTramite, IservicioAutorizacion autorizacion){
+public class CasoDeUsoTramiteBaja(ITramiteRepositorio repoTramite){
+    
+    // QUE ES MEJOR llamar al autorizacion aca o fuera de la clase en el main
+        // el IServicioAutorizacion autorizacion se pasa como primario
     public void Ejecutar(Tramite tramite){
-        if(autorizacion.PoseeElPermiso(1, Permiso permiso)){
+        //if(autorizacion.PoseeElPermiso(1, permiso)){
             repoTramite.eliminarTramite(tramite);
-        }
     }
 
     public void Ejecutar(int idE){
-        if(autorizacion.PoseeElPermiso(0, Permiso permiso)){
+        //if(autorizacion.PoseeElPermiso(0, Permiso permiso)){
             repoTramite.eliminarCompleto(idE);
-        }
     }
 }
