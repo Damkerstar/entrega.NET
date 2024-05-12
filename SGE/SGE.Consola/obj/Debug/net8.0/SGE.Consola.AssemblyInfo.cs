5f9c710432026930ef673db175b178d56b42876c
LeoStrange <lunaleonardo031@gmail.com>
2024-05-12 18:40:10 -0300

    asd

--- SGE/SGE.Consola/obj/Debug/net8.0/SGE.Consola.AssemblyInfo.cs
+++ SGE/SGE.Consola/obj/Debug/net8.0/SGE.Consola.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SGE.Consola")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56478b1157dd9120fd12e0261816b1413a8aa952")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1e6cbaac89d5814feb2de4c73a13aa5c3886934")]
 [assembly: System.Reflection.AssemblyProductAttribute("SGE.Consola")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SGE.Consola")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
