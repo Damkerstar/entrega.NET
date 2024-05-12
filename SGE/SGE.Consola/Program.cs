d43876d385ff9ca0acefc8e1871942d4acde5105
Damkerstar <damyoutu@gmail.com>
2024-05-12 18:50:28 -0300

    d

--- SGE/SGE.Consola/Program.cs
+++ SGE/SGE.Consola/Program.cs
@@ -5,16 +5,19 @@ class Program
 {
     public static void Main(string[] args)
     {
+        // Configurar dependencias
         IExpedienteRepositorio ExpedienteRepositorio = new RepositorioExpedienteTXT();
         ITramiteRepositorio TramiteRepositorio = new RepositorioTramiteTXT();
         ServicioActualizacionEstado ServicioActualizacion = new ServicioActualizacionEstado(ExpedienteRepositorio, new EspecificacionCambioEstado());
 
+        // Casos de Uso Expediente
         var BajaExpediente = new CasoDeUsoExpedienteBaja(ExpedienteRepositorio, TramiteRepositorio, new ServicioAutorizacionProvisorio());
         var AltaExpediente = new CasoDeUsoExpedienteAlta(ExpedienteRepositorio, new ExpedienteValidador(), new ServicioAutorizacionProvisorio());
         var TodosExpedientes = new CasoDeUsoExpedienteConsultaTodos(ExpedienteRepositorio);
         //var ExpedientesPorID = new CasoDeUsoExpedienteConsultaPorID(ExpedienteRepositorio);
         //var ModiExpediente = new CasoDeUsoExpedienteModificacion(ExpedienteRepositorio, new ServicioAutorizacionProvisorio());
 
+        // Casos de uso Tramite
         var BajaTramite = new CasoDeUsoTramiteBaja(TramiteRepositorio, new ServicioAutorizacionProvisorio(), ServicioActualizacion);
         var AltaTramite = new CasoDeUsoTramiteAlta(TramiteRepositorio, new TramiteValidador(), new ServicioAutorizacionProvisorio(), ServicioActualizacion);
         var TramitePorID = new CasoDeUsoTramiteConsultaPorEtiqueta(TramiteRepositorio);
@@ -22,14 +25,12 @@ class Program
     
         try
         {
-            Expediente expediente1 = new Expediente("Bertone", 1);
-            Expediente expediente2 = new Expediente("Thomas", 1);
-            Expediente expediente3 = new Expediente("Matias", 1);
+            // Dar de alta 3 expedientes
+            AltaExpediente.Ejecutar(new Expediente(){caratula = "Carátula del Expediente", ID = 1}, 1);
+            AltaExpediente.Ejecutar(new Expediente(){caratula = "Carátula del Expediente", ID = 1}, 1);
+            AltaExpediente.Ejecutar(new Expediente(){caratula = "Carátula del Expediente", ID = 1}, 1);
 
-            AltaExpediente.Ejecutar(expediente1, 1);
-            AltaExpediente.Ejecutar(expediente2, 1);
-            AltaExpediente.Ejecutar(expediente3, 1);
-
+            // Obtener una lista de los expedientes y imprimirla
             var lista = ExpedienteRepositorio.ListarExpedientes();   
         
             foreach(Expediente e in lista)
@@ -37,13 +38,10 @@ class Program
                 Console.WriteLine(e.ToString());
             }
 
-            Tramite tramite1 = new Tramite("AAAAH", 1, 1);
-            Tramite tramite2 = new Tramite("DDDD", 1, 1);
-            Tramite tramite3 = new Tramite("DORMIR", 1, 2);
-
-            AltaTramite.Ejecutar(tramite1, 1);
-            AltaTramite.Ejecutar(tramite2, 1);
-            AltaTramite.Ejecutar(tramite3, 1);
+            // Dar de alta 3 tramites y imprimirlo
+            AltaTramite.Ejecutar(new Tramite(){descripcion = "Descripción del Tramite", ID = 1, idE = 1}, 1);
+            AltaTramite.Ejecutar(new Tramite(){descripcion = "Descripción del Tramite", ID = 1, idE = 1}, 1);
+            AltaTramite.Ejecutar(new Tramite(){descripcion = "Descripción del Tramite", ID = 1, idE = 2}, 1);
 
             var listaTramite = TramiteRepositorio.ListarTramite();
 
@@ -52,6 +50,7 @@ class Program
                 Console.WriteLine(tramite);
             }
 
+            
         }
         catch (Exception ex)
         {
